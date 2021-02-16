9fa435d8ff4b817ea7ba8b5c1a2edd5e724c003a
bnmfw <bernardoborgessandoval@gmail.com>
2021-02-16 20:33:46 -0300

    funcao do boneco adicionada

---
+++ Ex-Forca-3.py
@@ -0,0 +1,10 @@
+def Renderizar_Boneco(vidas):
+    #FUNCAO ESPERA DE 0 A 5 LINHAS DE COGIDO
+    imagem = "forca_"+str(vidas)+".txt"
+    with open(imagem,"r") as boneco:
+        boneco = boneco.read()
+        print(boneco)
+
+
+vidas = int(input("Numero de vidas: "))
+Renderizar_Boneco(vidas)
