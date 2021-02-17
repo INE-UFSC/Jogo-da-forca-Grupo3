2f5c7ddd50728eb8739bb58de7ccd7ac856e6f2d
Arthur Lourenço <arthurjolo@gmail.com>
2021-02-17 08:21:58 -0300

    fiz o escreve palavra com as funções que pedia ali em escrito

--- Ex-Forca-3.py
+++ Ex-Forca-3.py
@@ -9,9 +9,35 @@ def Ler_Letra(palavra,letra,erros,acertos): #VITOR
     #ESSA FUNCAO LE UMA UNICA LETRA, CONFERE SE ELA EH VALIDA E VE SE EXISTE NA PALAVRA
     return [acertou]
 
-def Escreve_Palavra(palavra,letra,correta): #ARTHUR
+def Escreve_Palavra(palavra,letra,correta,erros,acertos,vidas): #ARTHUR
     #ATRIBUI A LETRA NOS ACERTOS OU ERROS
     #AQUI ELA ESCREVE AS COISA NA TELA
+    letra = letra.upper()
+    palavra = palavra.upper()
+    if correta == True:
+        for i in range(len(palavra)):
+            if palavra[i] == letra:
+                acertos[i] = letra
+    else:
+        erros.append(letra)
+        vidas = vidas-1
+        
+    Renderizar_Boneco(vidas)
+    print()
+    print(f'                ', end=" ")
+    for l in acertos:
+        print(f"{l}", end=" ")
+    
+    print()
+    print(f'\n             ', end=" ")
+    print("ERROS: ",end=" ")
+    for e in erros:
+        print(f"{e}", end=" ")
+    
+    print()
+    print(f'\n             ', end=" ")
+    print(f"Você ainda tem {vidas} chances!")
+
     return[erros,acertos]
 
 #Pessoa casa tudo (Otavio)
@@ -33,5 +59,5 @@ def Renderizar_Boneco(vidas): #Bernardo
 palavra = "palavra"
 erros = []
 acertos = [] #lista com os caracteres
-vidas = int(input("Numero de vidas: "))
+vidas = 5
 Renderizar_Boneco(vidas)
